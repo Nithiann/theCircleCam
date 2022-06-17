bebf60eb3bc6f9c15c7174b64d0b19496c7e2b61
bas <voss.bas@gmail.com>
2022-06-17 13:11:41 +0200

    streaming start

--- app/src/main/java/com/nithiann/thecircle/presentation/videopage/CameraView.kt
+++ app/src/main/java/com/nithiann/thecircle/presentation/videopage/CameraView.kt
@@ -1,8 +1,11 @@
 package com.nithiann.thecircle.presentation.videopage
 
+import android.R
 import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Build
 import android.util.Log
-import android.view.View
+import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
@@ -17,11 +20,19 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.PathUtils
 import com.nithiann.thecircle.R
+import com.pedro.rtmp.utils.ConnectCheckerRtmp
+import com.pedro.rtplibrary.rtmp.RtmpCamera1
+import java.io.IOException
 import java.util.concurrent.Executors
 
+
+@SuppressLint("StaticFieldLeak")
+private var context: Context? = null;
 @Composable
-fun CameraView() {
+fun CameraView(){
+    context = LocalContext.current;
     val lifecycleOwner = LocalLifecycleOwner.current
     val context = LocalContext.current
 
@@ -73,7 +84,73 @@ private class FaceAnalyzer(): ImageAnalysis.Analyzer {
     @SuppressLint("UnsafeOptInUsageError")
     override fun analyze(imageProxy: ImageProxy) {
         val image = imageProxy.image
-
+        RtmpCamera()
         image?.close()
     }
+}
+
+private fun RtmpCamera() {
+    val openGLView: OpenGLView = OpenGLView(context)
+    val connectCheckerRtmp: ConnectCheckerRtmp = com.nithiann.thecircle.presentation.videopage.ConnectCheckerRtmp();
+    val rtpmCamera: RtmpCamera1 = RtmpCamera1(openGLView, connectCheckerRtmp);
+    val folder = PathUtils
+    rtpmCamera.setReTries(10);
+
+    if (rtpmCamera.prepareAudio() && rtpmCamera.prepareVideo()) {
+        rtpmCamera.startStream("rtmp://88.198.76.192/live/DaddyAndroid")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            if (!rtpmCamera.isRecording()) {
+                try {
+                    if (!folder.exists()) {
+                        folder.mkdir()
+                    }
+                    val sdf = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault())
+                    currentDateAndTime = sdf.format(Date())
+                    if (!rtmpCamera1.isStreaming()) {
+                        if (rtmpCamera1.prepareAudio() && rtmpCamera1.prepareVideo()) {
+                            rtmpCamera1.startRecord(
+                                folder.getAbsolutePath()
+                                    .toString() + "/" + currentDateAndTime + ".mp4"
+                            )
+                            bRecord.setText(R.string.stop_record)
+                            Toast.makeText(this, "Recording... ", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(
+                                this, "Error preparing stream, This device cant do it",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    } else {
+                        rtpmCamera.startRecord(
+                            folder.getAbsolutePath().toString() + "/" + currentDateAndTime + ".mp4"
+                        )
+                        bRecord.setText(R.string.stop_record)
+                        Toast.makeText(this, "Recording... ", Toast.LENGTH_SHORT).show()
+                    }
+                } catch (e: IOException) {
+                    rtpmCamera.stopRecord()
+                    PathUtils.updateGallery(
+                        context,
+                        folder.getAbsolutePath().toString() + "/" + currentDateAndTime + ".mp4"
+                    )
+                    bRecord.setText(R.string.start_record)
+                    Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                rtmpCamera.stopRecord()
+                PathUtils.updateGallery(
+                    this,
+                    folder.getAbsolutePath().toString() + "/" + currentDateAndTime + ".mp4"
+                )
+                bRecord.setText(R.string.start_record)
+            }
+        } else {
+        }
+    } else {
+        // do nothing
+    }
+}
+
+private fun setRecording() {
+
 }
--- app/src/main/java/com/nithiann/thecircle/presentation/videopage/ConnectCheckerRtmp.kt
+++ app/src/main/java/com/nithiann/thecircle/presentation/videopage/ConnectCheckerRtmp.kt
@@ -1,4 +1,36 @@
 package com.nithiann.thecircle.presentation.videopage
 
-class ConnectCheckerRtmp {
+import android.util.Log
+import com.pedro.rtmp.utils.ConnectCheckerRtmp
+
+class ConnectCheckerRtmp: ConnectCheckerRtmp {
+    lateinit var streamingUrl: String
+
+    override fun onConnectionSuccessRtmp() {
+        Log.i("Stream", "Connection success")
+    }
+
+    override fun onDisconnectRtmp() {
+        Log.i("Stream", "Disconnected")
+    }
+
+    override fun onNewBitrateRtmp(bitrate: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAuthErrorRtmp() {
+        Log.e("Stream", "Authentication failed")
+    }
+
+    override fun onAuthSuccessRtmp() {
+        Log.i("Stream", "Authentication succeeded")
+    }
+
+    override fun onConnectionFailedRtmp(reason: String) {
+        Log.i("Stream", reason)
+    }
+
+    override fun onConnectionStartedRtmp(rtmpUrl: String) {
+        Log.i("Stream", "Connetion has started on " + rtmpUrl)
+    }
 }
