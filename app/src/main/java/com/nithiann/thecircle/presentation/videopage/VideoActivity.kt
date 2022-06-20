676a309a3e12276fc419b0b220e08a767349836f
Laptop <voss.bas@gmail.com>
2022-06-20 13:13:48 +0200

    video encoding and streaming works

--- app/src/main/java/com/nithiann/thecircle/presentation/videopage/VideoActivity.kt
+++ app/src/main/java/com/nithiann/thecircle/presentation/videopage/VideoActivity.kt
@@ -1,8 +1,6 @@
 package com.nithiann.thecircle.presentation.videopage
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.view.WindowManager
@@ -10,33 +8,45 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.core.graphics.PathUtils
 import com.nithiann.thecircle.R
-import com.nithiann.thecircle.common.PathUtils.recordPath
 import com.nithiann.thecircle.common.PathUtils.updateGallery
 import com.pedro.rtplibrary.rtmp.RtmpCamera1
+import java.io.IOException
+import java.text.SimpleDateFormat
 import java.util.*
+
 
 class VideoActivity: ComponentActivity(), SurfaceHolder.Callback {
     var rtmpCamera: RtmpCamera1? = null
-    val folder = null;
+    val folder = com.nithiann.thecircle.common.PathUtils.recordPath;
     //@SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        setContentView(R.layout.texture_layout)
-        val folder = com.nithiann.thecircle.common.PathUtils.recordPath
+        setContentView(com.nithiann.thecircle.R.layout.texture_layout)
         val connectCheckerRtmp: ConnectCheckerRtmp = ConnectCheckerRtmp()
-        val openGlView: OpenGLView = findViewById(R.id.surfaceView)
+        val openGlView: SurfaceView = findViewById(com.nithiann.thecircle.R.id.surfaceView)
         rtmpCamera = RtmpCamera1(openGlView, connectCheckerRtmp)
-        openGlView.getHolder().addCallback(this);
+        openGlView.holder.addCallback(this);
         //openGlView.setOnTouchListener(this);
     }
 
     override fun surfaceCreated(holder: SurfaceHolder) {
-        //do nothing
+        rtmpCamera?.startPreview()
     }
 
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-        rtmpCamera?.startPreview()
+        if (!rtmpCamera!!.isStreaming()) {
+            if (rtmpCamera!!.isRecording()
+                || rtmpCamera!!.prepareAudio() && rtmpCamera!!.prepareVideo()) {
+                rtmpCamera?.startStream("rtmp://88.198.76.192/live/DaddyAndroid")
+            } else {
+                Toast.makeText(this, "Error preparing stream, This device cant do it",
+                    Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            rtmpCamera?.stopStream();
+        }
+
     }
 
     override fun surfaceDestroyed(holder: SurfaceHolder) {
