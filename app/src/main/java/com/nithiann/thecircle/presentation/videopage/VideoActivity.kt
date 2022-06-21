1cedd3514da6aefd699e2a54b47b46080cb356e4
bas <voss.bas@gmail.com>
2022-06-21 11:52:58 +0200

    buttons on camera page for switching and starting camera

--- app/src/main/java/com/nithiann/thecircle/presentation/videopage/VideoActivity.kt
+++ app/src/main/java/com/nithiann/thecircle/presentation/videopage/VideoActivity.kt
@@ -20,6 +20,7 @@ import java.util.*
 class VideoActivity: ComponentActivity(), SurfaceHolder.Callback, View.OnClickListener {
     var rtmpCamera: RtmpCamera1? = null
     var sButton: Button? = null
+    var streamButton: Button? = null
     //@SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +31,8 @@ class VideoActivity: ComponentActivity(), SurfaceHolder.Callback, View.OnClickLi
         val openGlView: SurfaceView = findViewById(com.nithiann.thecircle.R.id.surfaceView)
         sButton = findViewById(R.id.switch_camera)
         sButton!!.setOnClickListener(this)
+        streamButton = findViewById(R.id.b_start_stop)
+        streamButton!!.setOnClickListener(this)
         rtmpCamera = RtmpCamera1(openGlView, connectCheckerRtmp)
         openGlView.holder.addCallback(this);
         //openGlView.setOnTouchListener(this);
@@ -40,17 +43,6 @@ class VideoActivity: ComponentActivity(), SurfaceHolder.Callback, View.OnClickLi
     }
 
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-        if (!rtmpCamera!!.isStreaming()) {
-            if (rtmpCamera!!.isRecording()
-                || rtmpCamera!!.prepareAudio() && rtmpCamera!!.prepareVideo()) {
-                rtmpCamera?.startStream("rtmp://88.198.76.192/live/DaddyAndroid")
-            } else {
-                Toast.makeText(this, "Error preparing stream, This device cant do it",
-                    Toast.LENGTH_SHORT).show();
-            }
-        } else {
-            rtmpCamera?.stopStream();
-        }
 
     }
 
@@ -68,7 +60,25 @@ class VideoActivity: ComponentActivity(), SurfaceHolder.Callback, View.OnClickLi
     override fun onClick(v: View?) {
         when(v?.id) {
             R.id.switch_camera -> rtmpCamera!!.switchCamera()
+            R.id.b_start_stop -> startStopStream()
         }
 
     }
+
+    private fun startStopStream() {
+        if (!rtmpCamera!!.isStreaming()) {
+            if (rtmpCamera!!.isRecording()
+                || rtmpCamera!!.prepareAudio() && rtmpCamera!!.prepareVideo()) {
+                rtmpCamera!!.startStream("rtmp://88.198.76.192:8000/live/developmentApp")
+                Toast.makeText(this, "Started streaming", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Error preparing stream, This device cant do it",
+                    Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            rtmpCamera?.stopStream();
+            Toast.makeText(this, "Stopped streaming", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
