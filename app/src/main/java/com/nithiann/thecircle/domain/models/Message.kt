89d972550c8d4d4af9080703732a44f6fae3b8e4
bas <voss.bas@gmail.com>
2022-06-28 13:18:00 +0200

    added message model design for new database

--- app/src/main/java/com/nithiann/thecircle/domain/models/Message.kt
+++ app/src/main/java/com/nithiann/thecircle/domain/models/Message.kt
@@ -1,10 +1,10 @@
 package com.nithiann.thecircle.domain.models
 
 
-// TODO: create based on new message design
 data class Message(
     val id: Int,
     val msg: String,
+    val msgSignature: String,
     val sentToStreamerId: Int,
     val sendByUserId: Int
 )
