208cd515acfeb209147f0715d2c8010ea572d26d
bas <voss.bas@gmail.com>
2022-06-29 16:06:37 +0200

    recycler view working

--- app/src/main/java/com/nithiann/thecircle/presentation/videopage/MessageAdapter.kt
+++ app/src/main/java/com/nithiann/thecircle/presentation/videopage/MessageAdapter.kt
@@ -7,8 +7,11 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.nithiann.thecircle.R
 import com.nithiann.thecircle.domain.models.Message
+import com.nithiann.thecircle.domain.models.MessageList
 
-class MessageAdapter(private val mList: List<Message>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class MessageAdapter(private val mList: MessageList?): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private lateinit var messageItem: TextView;
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.card_view_messages, parent, false)
@@ -16,18 +19,19 @@ class MessageAdapter(private val mList: List<Message>): RecyclerView.Adapter<Rec
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val ItemsViewModelMessages = mList[position]
+        val ItemsViewModelMessages = mList!!.messages[position]
+        messageItem.text = ItemsViewModelMessages.msg
     }
 
     override fun getItemCount(): Int {
-        return mList.size
+        return mList?.messages?.size ?: 0
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var messageItem: TextView
 
         init {
             messageItem = itemView.findViewById(R.id.textView_msg_card)
         }
     }
+
 }
--- app/src/main/java/com/nithiann/thecircle/presentation/videopage/MessageFragment.kt
+++ app/src/main/java/com/nithiann/thecircle/presentation/videopage/MessageFragment.kt
@@ -1,6 +1,7 @@
 package com.nithiann.thecircle.presentation.videopage
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,7 +17,9 @@ import androidx.lifecycle.get
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.nithiann.thecircle.R
+import com.nithiann.thecircle.common.Resource
 import com.nithiann.thecircle.domain.models.Message
+import com.nithiann.thecircle.domain.models.MessageList
 import com.nithiann.thecircle.domain.use_case.getMessagesUseCase
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -53,29 +56,20 @@ class MessageFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        recyclerView.apply {
-            layoutManager = LinearLayoutManager(activity)
+        viewModel.state.value
     }
 
-            val mList: List<Message> = listOf(Message(1, "send help", "hello world", 2, 2), Message(2, "to Amber", "hello world", 1, 1), Message(3, ":<", "hello world", 2, 2))
-            adapter = MessageAdapter(mList)
-        }
 
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.state.value
-            }
-        }
     override fun onStart() {
         super.onStart()
         val chat = viewModel.state.value
-        chat.messages?.messages?.forEach() { message ->
-            println(message ?: "no message")
-            textView.text = message.msg
-            println("+++++++++++++12345 " + message.msg)
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(activity)
+
+            val mList: List<Message> = listOf(Message(1, "send help", "hello world", 2, 2), Message(2, "to Amber", "hello world", 1, 1), Message(3, ":<", "hello world", 2, 2))
+            val messageList = MessageList(mList, "123123123")
+            adapter = MessageAdapter(chat.messages)
+            adapter?.notifyDataSetChanged()
         }
-        println("+++++++++++++-=-=-=- " + chat)
-        textView.text = chat.toString()
     }
 }
